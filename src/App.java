ee74fd867b8aaa99f6a8b63baca08a4c65558adb
romanlarrosa <58295696+romanlarrosa@users.noreply.github.com>
2020-11-26 15:27:46 +0100

    Created project

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.io.FileReader;
+
+import com.opencsv.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Vamos a leer un archivo");
+        String archCSV = "./files/movies.csv";
+        CSVReader csvReader = new CSVReader(new FileReader(archCSV));
+        String[] fila = null; //Lo estamos guardando aquí
+        while((fila = csvReader.readNext()) != null) {
+            System.out.println(fila[0]
+                    + " | " + fila[1]
+                    + " |  " + fila[2]);
+        }
+
+        csvReader.close();
+    }
+}
